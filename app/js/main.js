4a5c2bf7b11d2b0e0a9679f959ba2db73a00f0cb
Alex Antonov <antonov.mob@gmail.com>
2024-05-08 20:35:47 +0300

    1

--- app/js/main.js
+++ app/js/main.js
@@ -1,48 +1,90 @@
-$('.products__list').slick({
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    dots: false,
-    prevArrow: '<button class="slider__arrow prev-arrow"><img src="./images/slider-arrow-left.svg" alt="arrow"></button>',
-    nextArrow: '<button class="slider__arrow next-arrow"><img src="./images/slider-arrow-right.svg" alt="arrow"></button>',
-    responsive: [{
-        breakpoint: 1100,
-        settings: {
-            slidesToShow: 3,
+// $('.portfolio__list').slick({
+//     slidesToShow: 4,
+//     slidesToScroll: 2,
+//     dots: false,
+//     prevArrow: '<button class="slider__arrow prev-arrow"><img src="./images/slider-arrow-left.svg" alt="arrow"></button>',
+//     nextArrow: '<button class="slider__arrow next-arrow"><img src="./images/slider-arrow-right.svg" alt="arrow"></button>',
+//     responsive: [{
+//         breakpoint: 1100,
+//         settings: {
+//             slidesToShow: 3,
        
        
        
-      }
-    },
-    {
-        breakpoint: 850,
-        settings:{
-            slidesToShow: 2,
-            slidesToScroll: 2,
-           // centerPadding: '100px',
-        }
-    },
-    {
-        breakpoint: 620,
-        settings:{
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
+//       }
+//     },
+//     {
+//         breakpoint: 850,
+//         settings:{
+//             slidesToShow: 2,
+//             slidesToScroll: 2,
+//            // centerPadding: '100px',
+//         }
+//     },
+//     {
+//         breakpoint: 620,
+//         settings:{
+//             slidesToShow: 1,
+//             slidesToScroll: 1,
+//             arrows: false,
             
-            autoplay: true,
-            autoplaySpeed: 2500,
+//             autoplay: true,
+//             autoplaySpeed: 2500,
             
-        } 
-    },
+//         } 
+//     },
     
-    ]
-});
+//     ]
+// });
 
+
+const swiper = new Swiper('.image-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // autoplay: {
+    //   //delay: 2000,
+    //   stopOnLastSlide: true,
+    //   disableInteraction: false
+
+    // },
+    // speed: 800,
+    // autoHeight: true,
+     spaceBetween: 30,
+    // intialSlide: 3,
+  
+
+    
+    // cubeEffect: {
+    //   SlideShadow: true,
+    //   shadow: true,
+    //   shadowOffset: 20,
+    //   shadowScale: 0.94
+    // }
+    loop: true,  //infinity
+  });
+
+
+
+
+//-----------------------------------------------------------------------------------
 $('.header__burger').click(function(evt){
     $('.header__burger, .header__nav').toggleClass('active');
     // $('body').toggleClass('lock');
 })
 
-//-----------------------------------------------------------------------------------
+$('.header__link, .header__burger,').click(function(evt) {
+  $('.header__nav').toggleClass('active');
+})
+
+//---------------------------------------------------------------------------------------
+
 let map;
 
 
